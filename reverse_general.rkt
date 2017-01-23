6fbb403f1b4d29dd4f8ed4e58355bdc25bd114d6
Gauri Shankar Gautam <gaurishankargautam@Gauris-MacBook-Air.local>
2017-01-22 19:14:25 -0500

    Using the append function.

--- reverse_general.rkt
+++ reverse_general.rkt
@@ -1,6 +1,6 @@
 (define (reverse-general L)
   (cond
     ((NULL? L) L)
-    (not (NULL? L) CAR L))
-    (reverse-general (CDR L))
+    (not (NULL? L) append(list(reverse-general (CDR L)) list (CAR L)))
     )
+  )
