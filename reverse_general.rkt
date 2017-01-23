2f4d8f67aea0e8049b63fbd14d6c2fa431989a8b
Gauri Shankar Gautam <gaurishankargautam@Gauris-MacBook-Air.local>
2017-01-22 20:11:10 -0500

    Making the function work with general list by adding a new condition to check whether the element inside the list is a list.

--- reverse_general.rkt
+++ reverse_general.rkt
@@ -1,6 +1,8 @@
 (define (reverse-general L)
   (cond
     ((NULL? L) L)
+    ((list? (car L)) (append (list(reverse-general (CDR L))) (list(reverse-general(CAR L)) ) )) 
     (ELSE (append (reverse-general (CDR L)) (list (CAR L)) ) )
     )
   )
+
