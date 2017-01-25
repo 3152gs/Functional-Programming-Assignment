cb7233c54af225e1145ef8913f95992a73ba0a9f
Gauri Shankar Gautam <gaurishankargautam@Gauris-MacBook-Air.local>
2017-01-25 18:25:08 -0500

    Adding necessary comments to problem 2 and problem 3.

--- sum_up-_numbers_simple.rkt
+++ sum_up-_numbers_simple.rkt
@@ -1,8 +1,10 @@
 (define (sum-up-numbers-simple L)
     (cond
-      ((NULL? L) 0)
-      ((LIST? (car L)) (+ (sum-up-numbers-simple (cdr L)) 0))
-      ((not (number? (car L))) (+ (sum-up-numbers-simple (cdr L)) 0))
-      (else (+ (sum-up-numbers-simple (cdr L)) (car L)))
+      ((NULL? L) 0) ;Base case : Check if the function is empty. If empty add 0 to the sum.
+      ((LIST? (car L)) (+ (sum-up-numbers-simple (cdr L)) 0))   ;Check if list contains list. If there is a list make a recursive call 
+      															;for rest of the outer list and add 0 to the sum.
+                                                                
+      ((not (number? (car L))) (+ (sum-up-numbers-simple (cdr L)) 0))   ;;Check if the atom is a number or not. If not a number simply add 0 to the sum
+      (else (+ (sum-up-numbers-simple (cdr L)) (car L))) ; Recursively adding numbers to the sum.
       )
     )
--- sum_up_numbers_general.rkt
+++ sum_up_numbers_general.rkt
@@ -1,8 +1,9 @@
 (define (sum-up-numbers-general L)
     (cond
-      ((NULL? L) 0)
-      ((LIST? (car L)) (+ (sum-up-numbers-general (car L)) (cdr L))) ;change only on this line needed
-      ((not (number? (car L))) (+ (sum-up-numbers-general (car L)) 0))
-      (else (+ (sum-up-numbers-general (cdr L)) (car L)))
+      ((NULL? L) 0) ;Base case : Check if the function is empty. If empty add 0 to the sum.
+      ((LIST? (car L)) (+ (sum-up-numbers-general (car L))(sum-up-numbers-general (cdr L)) )) ;Check if list contains list. If there is a list recursively
+                                                                                              ;find sum of the car(list) and rest of the list
+      ((not (number? (car L))) (+ (sum-up-numbers-general (cdr L)) 0))   ;Check if the atom is a number or not. If not a number simply add 0 to the sum
+      (else (+ (sum-up-numbers-general (cdr L)) (car L))) ; Recursively adding numbers to the sum.
       )
     )
