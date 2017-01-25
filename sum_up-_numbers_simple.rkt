9af88a8c68d6ba127375143884aedfa72367ed0f
Gauri Shankar Gautam <gaurishankargautam@Gauris-MacBook-Air.local>
2017-01-25 17:15:57 -0500

    Now trying to filter out the nested lists. Error appears.

--- sum_up-_numbers_simple.rkt
+++ sum_up-_numbers_simple.rkt
@@ -1,7 +1,7 @@
 (define (sum-up-numbers-simple L)
     (cond
-      ((NULL? L) L)
-      ((LIST? car L) (+ sum-up-numbers-simple(cdr L) car L))
-      (+ (sum-up-numbers-simple (cdr L)) (car L))
+      ((NULL? L) 0)
+      ((LIST? car L) 0)
+      (else (+ (sum-up-numbers-simple (cdr L)) (car L)))
       )
     )
