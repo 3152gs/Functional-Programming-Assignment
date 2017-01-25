2d32946365f4ba93cd63ad40ee4bec5d9d9fa74f
Gauri Shankar Gautam <gaurishankargautam@Gauris-MacBook-Air.local>
2017-01-25 17:44:36 -0500

    Filtered out the nun-numbers by simply adding a new condition. FINAL ANSWER

--- sum_up-_numbers_simple.rkt
+++ sum_up-_numbers_simple.rkt
@@ -2,6 +2,7 @@
     (cond
       ((NULL? L) 0)
       ((LIST? (car L)) (+ (sum-up-numbers-simple (cdr L)) 0))
+      ((not (number? (car L))) (+ (sum-up-numbers-simple (cdr L)) 0))
       (else (+ (sum-up-numbers-simple (cdr L)) (car L)))
       )
     )
