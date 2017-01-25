48171cabe7448cce77d4e59cc65577536b9c38f2
Gauri Shankar Gautam <gaurishankargautam@Gauris-MacBook-Air.local>
2017-01-25 18:05:46 -0500

    Making the function add cdr(list) to the recursively called car(list). Not working yet.

--- sum_up_numbers_general.rkt
+++ sum_up_numbers_general.rkt
@@ -1,7 +1,7 @@
 (define (sum-up-numbers-general L)
     (cond
       ((NULL? L) 0)
-      ((LIST? (car L)) (+ (sum-up-numbers-general (car L)) 0)) ;change only on this line needed
+      ((LIST? (car L)) (+ (sum-up-numbers-general (car L)) (cdr L))) ;change only on this line needed
       ((not (number? (car L))) (+ (sum-up-numbers-general (car L)) 0))
       (else (+ (sum-up-numbers-general (cdr L)) (car L)))
       )
