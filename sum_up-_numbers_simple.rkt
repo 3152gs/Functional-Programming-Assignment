4353043d331002995b7dd3db768289f193c08ba6
Gauri Shankar Gautam <gaurishankargautam@Gauris-MacBook-Air.local>
2017-01-25 17:37:17 -0500

    FInally able to filter out the list by adding 0 to the recursive call. Next Goal filter non-numbers

--- sum_up-_numbers_simple.rkt
+++ sum_up-_numbers_simple.rkt
@@ -1,7 +1,7 @@
 (define (sum-up-numbers-simple L)
     (cond
       ((NULL? L) 0)
-      ;((LIST? (car L)) (+ (sum-up-numbers-simple (cdr L)) (car L)))  Ignore this line for now. Just see if the code works for a simple list only.
+      ((LIST? (car L)) (+ (sum-up-numbers-simple (cdr L)) 0))
       (else (+ (sum-up-numbers-simple (cdr L)) (car L)))
       )
     )
