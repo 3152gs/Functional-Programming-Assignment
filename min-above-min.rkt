0a24dfa6d5154ec6e01bbbac3266be4b347eb51f
Gauri Shankar Gautam <gaurishankargautam@Gauris-MacBook-Air.local>
2017-01-26 22:00:43 -0500

    Initial commit. Base cases.

---
+++ min-above-min.rkt
@@ -0,0 +1,8 @@
+;Find a minimum of a list first and then compare that minimum to the next list
+;Function to find the minimum
+(define (find-min L)
+    (cond
+      ((null? L) L)    ;if a list is empty then the minimum in that list is null
+      ((null? (cdr L)) (car L)) ;if the list has only one item then the only element is minimum
+      )
+    )
