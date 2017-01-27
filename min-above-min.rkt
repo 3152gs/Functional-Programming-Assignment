4032c15993520c4e76fc9cf04b56754ad38b5d95
Gauri Shankar Gautam <gaurishankargautam@Gauris-MacBook-Air.local>
2017-01-27 01:14:42 -0500

    Finding the minimum for all simple lists. Getting rid of the 'IF' inside COND.

--- min-above-min.rkt
+++ min-above-min.rkt
@@ -1,20 +1,14 @@
 ;Find a minimum of a list first and then compare that minimum to the next list
 ;Function to find the minimum
+;Find a minimum of a list first and then compare that minimum to the next list
+;Function to find the minimum
 (define (find-min L)
     (cond
       ((null? L) L)    ;if a list is empty then the minimum in that list is null
       ((not (number? (car L))) (find-min(cdr L))) ;if the item is non-number ignore it
-      (if (not (number? (find-min(cdr L))))
-          (car L) ;if a non-numeric character is returned, then the number before that has to returned to compare
-          (
-           ((null? (cdr L)) (car L)) ;if the list has only one item then the only element is minimum
-           ((< (car L)(find-min (cdr L))) ; Compare the values
-          (car L)
-          
-          )
-           )
-          )
-          
+      ((not (number? (find-min(cdr L))))(car L)) ;if a non-numeric character is returned, then the number before that has to returned to compare
+      ((null? (cdr L)) (car L)) ;if the list has only one item then the only element is minimum
+      ((< (car L)(find-min (cdr L)))(car L)) ; Compare the values         
       (else (find-min(cdr L)))
       )
     )
