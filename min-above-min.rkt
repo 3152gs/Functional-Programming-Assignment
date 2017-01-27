8ec92bd2bbc1f914a7aebed25f9df686a63acba4
Gauri Shankar Gautam <gaurishankargautam@Gauris-MacBook-Air.local>
2017-01-27 02:51:57 -0500

    change in min-above-min.rkt

--- min-above-min.rkt
+++ min-above-min.rkt
@@ -1,37 +1,3 @@
-;Question 1
-(define (reverse-general L)
-  (cond
-    ((NULL? L) L) ; Checking if the list is null or not
-    ((list? (car L))  (cons (reverse-general (CDR L)) (list(reverse-general(CAR L)) )) ) ;checking if the element inside the list is a list or not.
-                                                                                         ;If yes, reverse the second element first and then reverse the first element.
-    (ELSE (append (reverse-general (CDR L)) (list (CAR L)) ) ) ;If this is a simple list reverse the list by recursively passing CDR(list).
-    )
-  )
-
-
-;Question 2
-(define (sum-up-numbers-simple L)
-    (cond
-      ((NULL? L) 0) ;Base case : Check if the function is empty. If empty add 0 to the sum.
-      ((LIST? (car L)) (+ (sum-up-numbers-simple (cdr L)) 0))   ;Check if list contains list. If there is a list make a recursive call 
-      															;for rest of the outer list and add 0 to the sum.
-                                                                
-      ((not (number? (car L))) (+ (sum-up-numbers-simple (cdr L)) 0))   ;;Check if the atom is a number or not. If not a number simply add 0 to the sum
-      (else (+ (sum-up-numbers-simple (cdr L)) (car L))) ; Recursively adding numbers to the sum.
-      )
-    )
-
-;Question 3
-(define (sum-up-numbers-general L)
-    (cond
-      ((NULL? L) 0) ;Base case : Check if the function is empty. If empty add 0 to the sum.
-      ((LIST? (car L)) (+ (sum-up-numbers-general (car L))(sum-up-numbers-general (cdr L)) )) ;Check if list contains list. If there is a list recursively
-                                                                                              ;find sum of the car(list) and rest of the list
-      ((not (number? (car L))) (+ (sum-up-numbers-general (cdr L)) 0))   ;Check if the atom is a number or not. If not a number simply add 0 to the sum
-      (else (+ (sum-up-numbers-general (cdr L)) (car L))) ; Recursively adding numbers to the sum.
-      )
-    )
-
 ;Question 4
 ;Find a minimum of a list first and then compare that minimum to the next list
 ;Function to find the minimum
